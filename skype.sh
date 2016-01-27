9ac6483778dabf9795d79ddd6fb1a09b46cf418e
Brian Aker <brian@tangent.org>
2016-01-27 12:03:40 -0800

    Skype preview?

--- skype.sh
+++ skype.sh
@@ -5,6 +5,7 @@ defaults write com.skype.skype AutoCollapseSidebar -bool false
 defaults write com.skype.skype DisableWebKitDeveloperExtras -bool false
 defaults write com.skype.skype IncludeDebugMenu -bool true
 defaults write com.skype.skype SKDisableWelcomeTour -bool true
+defaults write com.skype.skype SKFileTransferShowImagesInline -bool false
 defaults write com.skype.skype SKMyselfCorner 3
 defaults write com.skype.skype SKMyselfMinimized -bool false
 defaults write com.skype.skype SKShowWelcomeTour -bool false
