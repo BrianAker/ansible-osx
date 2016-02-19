9e707672f3614b1f2e5d131f86dd0ab345218bba
Brian Aker <brian@tangent.org>
2016-02-19 00:42:40 -0800

    Small changes.

--- com.apple.Safari.sh
+++ com.apple.Safari.sh
@@ -1,2 +1,4 @@
 #
 defaults write com.apple.Safari UserStyleSheetLocationURLString -string "~/.safari/default.css"
+defaults write com.apple.Safari AlwaysShowTabBar -bool true
+defaults write com.apple.Safari Command1Through9SwitchesTabs -bool true
