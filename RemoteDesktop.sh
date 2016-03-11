b352d54b7aabfb3b131ce9d524d5616ef8b3e8b6
Brian Aker <brian@tangent.org>
2016-03-11 00:11:28 -0800

    Fix Remote to be ansible

--- RemoteDesktop.sh
+++
@@ -1,7 +0,0 @@
-defaults write com.apple.RemoteDesktop doubleClickAction -string "Control"
-defaults write com.apple.RemoteDesktop encryptCopy -bool false
-defaults write com.apple.RemoteDesktop encryptInstall -bool false
-defaults write com.apple.RemoteDesktop sendTaskNotification -bool false
-defaults write com.apple.RemoteDesktop serverGroupOpen -bool false
-defaults write com.apple.RemoteDesktop useEncryptedTunnel -bool false
-defaults write com.apple.RemoteDesktop useKeychain -bool false
